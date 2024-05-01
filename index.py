488e3a396d92a81bd494240ea271c237a011b174
aagude18 <alexagucar2022@gmail.com>
2024-05-01 12:49:20 -0500

    Se adicionan enlaces y reorganizan nuevos campos como el de observaciones y se crea el formulario

--- index.py
+++ index.py
@@ -3,22 +3,22 @@ from flask import Flask, render_template # Render Template es para redireccionar
 app = Flask(__name__) # Inicia flask y lo almacena en una variable
 
 # Creating simple Routes 
-@app.route('/test')
-def test():
-    return "Home Page"
+#@app.route('/test')
+#def test():
+#    return "Home Page"
 
-@app.route('/test/about/')
-def about_test():
-    return "About Page"
+#@app.route('/test/about/')
+#def about_test():
+#    return "About Page"
 
 # Routes to Render Something
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/about', strict_slashes=False)
-def about():
-    return render_template("about.html")
+@app.route('/enlaces', strict_slashes=False)
+def enlaces():
+    return render_template("enlaces.html")
 
 # Make sure this we are executing this file
 if __name__ == '__main__':
