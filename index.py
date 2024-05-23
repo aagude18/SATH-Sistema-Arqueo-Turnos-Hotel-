cd42508cc30ff393a2af1c227f70255cb9a4dcb4
aagude18 <alexagucar2022@gmail.com>
2024-05-23 11:42:02 -0500

    Se crea Primera parte del CRUD en FLASK

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template # Render Template es para redireccionar las rutas a los template HTML
-
+import os
+import src.database as db
+template_dir =os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'src', 'templates')
 app = Flask(__name__) # Inicia flask y lo almacena en una variable
 
 # Creating simple Routes 
@@ -14,7 +17,16 @@ app = Flask(__name__) # Inicia flask y lo almacena en una variable
 # Routes to Render Something
 @app.route('/')
 def home():
-    return render_template("home.html")
+    cursor = db.mydb.cursor()
+    cursor.execute("SELECT * FROM users")
+    myresult = cursor.fetchall()
+    #Convertir a Diccionario
+    insertObject = []
+    columnName = [column[0] for column in cursor.description]
+    for recrod in myresult:
+        insertObject.append(dict(zip(columnName, recrod)))
+    cursor.close()
+    return render_template("index.html", data=insertObject)
 
 @app.route('/enlaces', strict_slashes=False)
 def enlaces():
