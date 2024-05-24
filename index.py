560d5ebc4a10401b1eef0c0e5aeb4935f6bb7f5f
aagude18 <alexagucar2022@gmail.com>
2024-05-23 19:46:16 -0500

    Se termina CRUD

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template # Render Template es para redireccionar las rutas a los template HTML
+from flask import Flask, render_template, request, redirect, url_for # Render Template es para redireccionar las rutas a los template HTML
 import os
 import src.database as db
 template_dir =os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
@@ -27,6 +27,45 @@ def home():
         insertObject.append(dict(zip(columnName, recrod)))
     cursor.close()
     return render_template("index.html", data=insertObject)
+
+# Ruta para guardar Usuarios en La BD
+@app.route('/user', methods=['POST'])
+def addUser():
+    username = request.form['Username']
+    name = request.form['Name']
+    password = request.form['Password']
+    if username and name and password:
+        cursor = db.mydb.cursor()
+        sql = "INSERT INTO users (Username, Firstname, Passw) VALUES (%s, %s, %s)"
+        data = (username, name, password)
+        cursor.execute(sql, data)
+        db.mydb.commit()
+    return redirect(url_for('home'))
+
+#Ruta Para Eliminar
+@app.route('/delete/<string:id>')
+def delete(id):
+    cursor = db.mydb.cursor()
+    sql = "DELETE FROM users WHERE id=%s"
+    data = (id,)
+    cursor.execute(sql, data)
+    db.mydb.commit()
+    return redirect(url_for('home'))
+
+
+#Ruta Para Eliminar
+@app.route('/edit/<string:id>', methods=['POST'])
+def edit(id):
+    username = request.form['Username']
+    name = request.form['Name']
+    password = request.form['Password']
+    if username and name and password:
+        cursor = db.mydb.cursor()
+        sql = "UPDATE users SET Username =%s, Firstname =%s, Passw =%s WHERE Id =%s"
+        data = (username, name, password, id)
+        cursor.execute(sql, data)
+        db.mydb.commit()
+    return redirect(url_for('home'))
 
 @app.route('/enlaces', strict_slashes=False)
 def enlaces():
